921ea8ed1ff39f1992c909d171807d02c21a911d
amitaymx <amitaymxd@gmail.com>
2023-06-25 19:06:58 +0300

    added zircon

--- src/main/java/net/amitay/tutorialmod/TutorialMod.java
+++ src/main/java/net/amitay/tutorialmod/TutorialMod.java
@@ -1,6 +1,8 @@
 package net.amitay.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.amitay.tutorialmod.block.ModBlocks;
+import net.amitay.tutorialmod.item.ModItems;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -20,6 +22,10 @@ public class TutorialMod
     public TutorialMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
+
+        ModBlocks.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
